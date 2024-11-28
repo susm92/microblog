b92382dc37702c772e75b09fda47bbba83f20c4e
susm92 <sucrosmrkovic92@gmail.com>
2024-11-28 21:57:09 +0100

    added test for version control

---
+++ tests/integration/main/test_version_check.py
@@ -0,0 +1,12 @@
+"""
+Test routes for version control
+"""
+# pylint: disable=redefined-outer-name,unused-argument
+
+def test_version_control_route(client):
+    """
+    Test that version route exists.
+    """
+    response = client.get("/version")
+    assert response.status_code == 200
+    assert b"Current version!" in response.data
