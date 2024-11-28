daf3df5370a01160fd2e2fcbf1df83a5e31a5c3f
Sucro Smrkovic <80321350+susm92@users.noreply.github.com>
2024-11-28 20:05:41 +0100

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, current_app
 from flask_login import current_user, login_required
-from app import db, version
+from app import db
 from app.main.forms import EditProfileForm, PostForm
 from app.models import User, Post
 from app.main import bp
